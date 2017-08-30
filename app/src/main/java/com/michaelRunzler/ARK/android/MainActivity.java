f5f8aeac3541442b977fd41813c7b7969fd83039
Ethan <ap792a@gmail.com>
2017-08-30 00:52:07 -0700

    Initial commit

---
+++ app/src/main/java/com/michaelRunzler/ARK/android/MainActivity.java
@@ -0,0 +1,25 @@
+package com.michaelRunzler.ARK.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+
+    public void activateMenuSlideout(View view) {
+    }
+
+    public void showSettings(View view) {
+    }
+
+    public void showMainHelp(View view) {
+    }
+}
